5ae35d5b6459ced1dcda37b5bf1f99356768d6de
Xavier Perez <xperez007@gmail.com>
2020-01-28 18:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,16 @@ def long_planeteer_calls
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+#> "gouda"
+
+soup = [tomato soup", "cheddar", "oyster crackers", "gouda"}
+find_the_cheese(soup)
+#=> "cheddar"
+
+code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
